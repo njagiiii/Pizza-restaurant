f514d92cbc9c9cb6ff5aa8a4fd9fa13de2bfd560
njagiiii <grace.makena@student.moringaschool.com>
2023-09-30 18:05:00 -0400

    Adde routs testing

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app= Flask(__name__)
+
 app.config['SECRET_KEY'] = '7e627ef3576afa7c10e1605bc1592d25'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurant.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
---
+++ testing/routes_test.py
@@ -0,0 +1,22 @@
+from myapp import app
+
+class TestApp:
+    def test_home_view(self):
+     client = app.test_client(self)
+     response = client.get("/")
+     assert response.status_code == 200
+
+    def test_restaurant_view(self):
+     client = app.test_client(self)
+     response = client.get("/restaurant")
+     assert response.status_code == 200
+
+    def test_pizza_view(self):
+     client = app.test_client(self)
+     response = client.get("/pizza")
+     assert response.status_code == 200
+
+    def test_restaurant_pizza_view(self):
+     client = app.test_client(self)
+     response = client.get('/restaurant_pizzas')
+     assert response.status_code == 200
